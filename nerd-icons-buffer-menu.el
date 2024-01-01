7cd060403f97c1e6cd0c6287eefadc0743189d37
Jen-Chieh Shen <jcs090218@gmail.com>
2024-01-01 01:33:29 -0800

    chore: Update copyright year

--- nerd-icons-buffer-menu.el
+++ nerd-icons-buffer-menu.el
@@ -1,6 +1,6 @@
 ;;; nerd-icons-buffer-menu.el --- Display nerd icons in `buffer-menu'  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023  Shen, Jen-Chieh
+;; Copyright (C) 2023-2024  Shen, Jen-Chieh
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
